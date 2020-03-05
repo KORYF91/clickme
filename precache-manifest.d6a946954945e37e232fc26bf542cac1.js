efe937baa1a5fab865d941e65cdf347c4809ca2f
Kfriedman <Koryf91@gmail.com>
2020-03-04 22:43:08 -0800

    Updates

---
+++ precache-manifest.d6a946954945e37e232fc26bf542cac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88727b9591d7e2fa6f9176747be94323",
+    "url": "/clickme/index.html"
+  },
+  {
+    "revision": "47856e6ca157614ff555",
+    "url": "/clickme/static/js/2.6853457e.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/clickme/static/js/2.6853457e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73dbd003738659361ad",
+    "url": "/clickme/static/js/main.4c6a4032.chunk.js"
+  },
+  {
+    "revision": "347bf23b2bc38013efa5",
+    "url": "/clickme/static/js/runtime-main.83aab2db.js"
+  }
+]);
